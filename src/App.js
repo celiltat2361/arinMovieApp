3e3759caf099784b4ed3580b61057f73ab39a8fb
celiltat2361 <celilbtc@gmail.com>
2021-08-04 19:48:45 +0200

    delete an item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList'
@@ -8,13 +7,6 @@ class App extends React.Component {
 
     state = {
       movies: [
-        {
-            "id": 1,
-            "name": "The Flash",
-            "rating": 8.3,
-            "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-            "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-        },
     
         {
             "id": 2,
